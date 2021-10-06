b9115a94be4006bfa6b27e82aa5d55fdacc1e907
MateoRincon04 <marinconar@unal.edu.co>
2021-10-06 14:36:50 -0500

    first step done

--- dist/protractor/local.config.js
+++ dist/protractor/local.config.js
@@ -4,6 +4,13 @@ exports.config = void 0;
 exports.config = {
     framework: 'jasmine',
     specs: ['../test/google.spec.js'],
-    seleniumAddress: 'http://localhost:4444/wd/hub'
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: {
+        browserName: 'chrome',
+        count: 1,
+        chromeOptions: {
+            args: ['--headless'],
+        },
+    },
 };
 //# sourceMappingURL=local.config.js.map
